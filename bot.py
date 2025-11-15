cde8b2c943c5858bae759973df698a548ef03afb
DrWhite <68634413+DrWhiteTG@users.noreply.github.com>
2025-11-15 11:20:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import sys
 import glob
 import importlib
 from pathlib import Path
-from pyrogram import Client, idle, __version__
+from pyrogram import idle, __version__
 from pyrogram.raw.all import layer
 import time
 from pyrogram.errors import FloodWait
@@ -39,12 +39,15 @@ botStartTime = time.time()
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 
-async def dreamxbotz_start():
-    print('\n\nInitalizing DreamxBotz')
+async def main():
+    print('\n\nInitializing DreamxBotz')
     await dreamxbotz.start()
+    
     bot_info = await dreamxbotz.get_me()
     dreamxbotz.username = bot_info.username
+    
     await initialize_clients()
+    
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
@@ -56,39 +59,51 @@ async def dreamxbotz_start():
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
             print("DreamxBotz Imported => " + plugin_name)
+    
     if ON_HEROKU:
-        asyncio.create_task(ping_server()) 
+        asyncio.create_task(ping_server())
+    
     b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
+    
     await Media.ensure_indexes()
     if MULTIPLE_DB:
         await Media2.ensure_indexes()
         print("Multiple Database Mode On. Now Files Will Be Save In Second DB If First DB Is Full")
     else:
         print("Single DB Mode On ! Files Will Be Save In First Database")
+    
     me = await dreamxbotz.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
     temp.B_LINK = me.mention
     dreamxbotz.username = '@' + me.username
+    
     dreamxbotz.loop.create_task(check_expired_premium(dreamxbotz))
+    
     logging.info(f"{me.first_name} with Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
     logging.info(LOG_STR)
     logging.info(script.LOGO)
+    
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     now = datetime.now(tz)
-    time = now.strftime("%H:%M:%S %p")
-    await dreamxbotz.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.B_LINK, today, time))
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-    dreamxbotz.loop.create_task(keep_alive())
-    await idle()
+    curr_time = now.strftime("%H:%M:%S %p")
+    await dreamxbotz.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.B_LINK, today, curr_time))
     
+    app = await web_server()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    bind_address = "0.0.0.0"
+    site = web.TCPSite(runner, bind_address, PORT)
+    await site.start()
+    
+    dreamxbotz.loop.create_task(keep_alive())
+    
+    await idle()
+
 if __name__ == "__main__":
     logger.info("Started Filter Bot")
-    app.run()
+    asyncio.run(main())
