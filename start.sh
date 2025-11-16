ad77cf6945ac011bfbe76687c6a6bf4a4b794f4a
DrWhite <68634413+DrWhiteTG@users.noreply.github.com>
2025-11-16 16:13:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gunicorn app:app --bind 0.0.0.0:8000 &  # run gunicorn in background
+gunicorn app:app --bind 0.0.0.0:8080 &  # run gunicorn in background
 python3 bot.py     # run main.py in foreground
